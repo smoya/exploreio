8bcfd038debee8a2179f03005c2cf76bbbd5b919
Sergio Moya <smoya89@gmail.com>
2017-01-21 15:42:25 +0100

    SOLUTION s07a

--- s07a/main.go
+++ s07a/main.go
@@ -4,6 +4,17 @@
 //    Reflection is never clear.
 package main
 
+import (
+	"io"
+	"os"
+	"github.com/prometheus/common/log"
+)
+
 func main() {
 	// TODO: Only read the first 27 bytes of stdin. 3 (or 6) lines with error handling.
+	r := io.LimitReader(os.Stdin, 27)
+	_, err := io.Copy(os.Stdout, r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
