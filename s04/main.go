f2861c369caaf77f6669cc282108ace72f17105f
Sergio Moya <smoya89@gmail.com>
2017-01-21 15:26:34 +0100

    SOLUTION s04

--- s04/main.go
+++ s04/main.go
@@ -4,7 +4,24 @@
 //     Don't just check errors, handle them gracefully.
 package main
 
+import (
+	"compress/gzip"
+	"io"
+	"os"
+
+	"github.com/prometheus/common/log"
+)
+
 func main() {
 	// TODO: Read compressed input from stdin and pass it to Stdout.
 	// TODO: without using a byte slice (7 lines, including error handling).
+	reader, err := gzip.NewReader(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = io.Copy(os.Stdout, reader)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
