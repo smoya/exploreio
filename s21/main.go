9e4c81bd1745a079ad02ede90a49d612d32daca0
Sergio Moya <smoya89@gmail.com>
2017-01-21 16:54:25 +0100

    SOLUTION s21

--- s21/main.go
+++ s21/main.go
@@ -8,21 +8,25 @@ import (
 	"io"
 	"log"
 	"os"
+	"bytes"
 )
 
 // TODO: Implement UpperReader, a reader that converts all Unicode letter mapped to their upper case. 11 lines.
-// ...
-// ...
-// ...
+type UpperReader struct {
+	r io.Reader
+}
 
-// ...
-// ...
-// ...
-// ...
-// ...
-// ...
-// ...
-// ...
+func (u UpperReader) Read(p []byte) (n int, err error) {
+	n, err = u.r.Read(p)
+	if err != nil {
+		return
+	}
+
+	copy(p, bytes.ToUpper(p))
+
+	return
+}
+
 func main() {
 	_, err := io.Copy(os.Stdout, &UpperReader{r: os.Stdin})
 	if err != nil {
