6509befe6c0b82d977b1af79e4c925efcb40135b
Sergio Moya <smoya89@gmail.com>
2017-01-21 15:41:32 +0100

    SOLUTION s06

--- s06/main.go
+++ s06/main.go
@@ -8,6 +8,9 @@ package main
 import (
 	"fmt"
 	"time"
+	"encoding/json"
+	"os"
+	"github.com/prometheus/common/log"
 )
 
 type record struct {
@@ -18,5 +21,11 @@ type record struct {
 
 func main() {
 	// TODO: Unmarshal from Stdin into a record struct. 4 lines with error handling.
+	var rec record
+	err := json.NewDecoder(os.Stdin).Decode(&rec)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Printf("It's around %s now and we are at wonderful %s in %s! @golab_conf\n", rec.Date, rec.Name, rec.Location)
 }
