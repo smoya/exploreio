646a12d2cdfea9460274ad55bb173dc85c39e127
Sergio Moya <smoya89@gmail.com>
2017-01-21 15:22:06 +0100

    SOLUTION s03

--- s03/main.go
+++ s03/main.go
@@ -6,7 +6,17 @@
 //     Cgo is not Go.
 package main
 
+import (
+	"io"
+	"os"
+	"github.com/prometheus/common/log"
+)
+
 func main() {
+	_, err := io.Copy(os.Stdout, os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
 	// TODO: Read input from stdin and pass it to Stdout.
 	// TODO: without using a byte slice (3 lines, including error handling).
 }
