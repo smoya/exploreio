4f47784000664d1b94d89cd3c0ddeb1624492289
Sergio Moya <smoya89@gmail.com>
2017-01-21 15:49:53 +0100

    SOLUTION s09

--- s09/main.go
+++ s09/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"io"
 )
 
 func main() {
@@ -15,9 +16,11 @@ func main() {
 	r := rand.New(rand.NewSource(123))
 
 	// TODO: Copy 24 byte from random source into the encoder. 3 lines including error handling.
-	// ...
-	// ...
-	// ...
+
+	_, err := io.CopyN(encoder, r, 24)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Note: The encoder has to be closed to write any partial data.
 	if err := encoder.Close(); err != nil {
